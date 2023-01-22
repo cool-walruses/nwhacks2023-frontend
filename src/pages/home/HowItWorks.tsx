555c27033f1dd2436f7b83c9f4862154718409c8
Ruby Yang <66763470+Ruby-ry@users.noreply.github.com>
2023-01-22 11:39:50 -0800

    Push terminal animation

--- src/pages/home/HowItWorks.tsx
+++ src/pages/home/HowItWorks.tsx
@@ -83,7 +83,33 @@ const HowItWorks: React.FC = () => {
           header="Ask what you’re really thinking"
           body="Prompt the model in the language that you are most comfortable with. We use a state-of-the-art language translation model to allow you to ask for and receive code in your native language without having to go through the trouble of learning another language to interpret code and search for solutions. Accessibility to information is no longer a concern with a tool that can gather and present code in a way that matches your preferences."
         />
-        <Image />
+        <div>
+          <FadeIn>
+            <div css={css`
+              transform: scale(0.85);
+            `}>
+              <img src="terminal.png"/>
+            </div>
+            <div css={css`
+              width: 400px;
+              height: 100px;
+              font-family: 'input mono';
+              font-size: 30px;
+              font-weight: bold;
+              text-align: left;
+              color: green;
+              top: 17px;
+            `}>
+              <span style={
+                {
+                  color: "gray"
+                }
+              }># Un programme pour dire bonjour au monde</span>
+              <br></br>
+              <span><Scramble text={"print(\"Bonjour le monde!\")"}/></span>
+            </div>
+          </FadeIn>
+        </div>
       </Aligner>
 
     </div>
