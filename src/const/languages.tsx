ee2d074696bd4c977fd1d0fa6ebe9a1b6f3fd870
Ruby Yang <66763470+Ruby-ry@users.noreply.github.com>
2023-01-21 22:59:12 -0800

    Add supported PL enum

---
+++ src/const/languages.tsx
@@ -0,0 +1,12 @@
+export enum programming_languages {
+    PYTHON = "Python",
+    JAVA = "Java",
+    JS = "JavaScript",
+    C_SHARP = "C#",
+    CPP = "C++",
+    C = "C",
+    GO = "Golang",
+    PHP = "PHP",
+    HTML = "HTML",
+    CSS = "CSS"
+}
