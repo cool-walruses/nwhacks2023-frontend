fc2966b1b297fcfb8dbf47223fbe8fcd4a92f781
Ruby Yang <66763470+Ruby-ry@users.noreply.github.com>
2023-01-21 20:53:38 -0800

    Update Chat.tsx

--- src/pages/convert/Chat.tsx
+++ src/pages/convert/Chat.tsx
@@ -1,58 +1,55 @@
-import Grid from "@mui/material/Grid"
-import ChatBox from "../../components/ChatBox"
-import { fontFamily, height, padding } from "@mui/system"
+import { css } from "@emotion/react"
 
 const Chat: React.FC = () => {
   return (
-    <div style={
-        {
-            paddingTop: "100px",
-            paddingLeft: "30px",
-            paddingRight: "30px",
-            height: "100vh",
-        }
-    }>
-        <div style={
-            {
-                height: "100%",
-                width: "100%",
-                backgroundColor: "#F5F5F5",
-                borderRadius: "12px",
-                display: "flex",
-                flexDirection: "column"
-            }
+    <div
+        css={css`
+            padding-top: 100px;
+            padding-left: 30px;
+            padding-right: 30px;
+            height: 100vh;
+        `}
+        >
+        <div css={css`
+                height: 100%;
+                width: 100%;
+                background-color: #F5F5F5;
+                border-radius: 12px;
+                display: flex;
+                flex-direction: column;
+            `
         }>
-            <div style={
-                {
-                    flex: "1",
-                    overflowY: "scroll",
-                }
+            <div css={css`
+                    flex: 1;
+                    overflow-y: scroll;
+                `
             }>
-                {/* <div style={{
-                    height: "200vh"
-                }}>a
-                </div> */}
+                <div css={css`
+                    height: 100vh;
+                `}>
+
+                </div>
             </div>
-            <div style = {
-                {
-                    padding: "30px",
-                    // backgroundColor: "red"
-                }
+            <div css={css`
+                    padding: 30px;
+                    // backgroundColor: "red";
+                `
             }>
-                <input type="text" style = {
-                    {
-                        width: "100%",
-                        margin: "auto",
-                        paddingTop: "10px",
-                        paddingBottom: "10px",
-                        paddingLeft: "10px",
-                        paddingRight: "10px",
-                        boxSizing: "border-box",
-                        backgroundColor: "#E3E3E3",
-                        fontFamily: "input mono",
-                        borderRadius: "7px",
-                        boxShadow: "2px 4px #959595"
-                    }
+                <input type="text" css={css`
+                        width: 100%;
+                        height: 75px;
+                        text-align: top;
+                        margin: auto;
+                        padding-top: 5px;
+                        padding-bottom: 10px;
+                        padding-left: 10px;
+                        padding-right: 10px;
+                        box-sizing: border-box;
+                        background-color: #E3E3E3;
+                        font-family: input mono;
+                        border-radius: 7px;
+                        box-shadow: 2px 4px #959595;
+                    `
                 } />
             </div>
         </div>
