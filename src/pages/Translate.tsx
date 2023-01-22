7acf14c6fdabb341f910a36f36219971d6909307
Ruby Yang <66763470+Ruby-ry@users.noreply.github.com>
2023-01-21 23:09:38 -0800

    Add dropdown component to translate

--- src/pages/Translate.tsx
+++ src/pages/Translate.tsx
@@ -1,11 +1,13 @@
 import Chat from "./translate/Chat"
 import NavBar from "./home/NavBar"
+import Dropdown from "./translate/Dropdown"
 
 const Translate: React.FC = () => {
   return (
     <>
       <NavBar />
       <Chat />
+      <Dropdown />
     </>
   )
 }
---
+++ src/pages/translate/Dropdown.tsx
@@ -0,0 +1,10 @@
+import { css } from "@emotion/react"
+
+const Dropdown: React.FC = () => {
+  return (
+    <div> Dropdown menu here
+    </div>
+  )
+}
+
+export default Dropdown
